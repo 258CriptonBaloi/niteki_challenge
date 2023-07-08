b17e89af4846ba05327733cfe5549661111c1032
Cripton Baloi <72799525+258CriptonBaloi@users.noreply.github.com>
2023-07-07 22:56:39 -0700

    menu logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -27,10 +27,13 @@ function Menu(){
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              <img src='./images/logoazul.png' width={98} height={92} alt='logotipo'/>
+          </Link>
 
-        <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-        </div>
+          <div className='menu-icon' onClick={handleClick}>
+              <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
 
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
